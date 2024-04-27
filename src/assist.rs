ab7a87bbdd05e352e348c33b24c60942474bb482
gentleGuardian123 <takigawa_yoshino@163.com>
2024-04-27 18:25:02 +0800

    added callgraph, replace all unwrap.

--- src/assist.rs
+++ src/assist.rs
@@ -1,10 +1,13 @@
+use std::io::Write;
 use std::process::Command;
 use std::path::Path;
+use std::fs::File;
 
 const _ASSIST_DIR: &str = "assists/";
 const _TMP_INS: &str = "_instrumentation";
 const _TMP_CG: &str = "_callgraph";
 const _TMP_PS: &str = "_path_sensitive";
+const _LINKED_LL: &str = "_linked_ll.ll";
 
 pub enum AssistProgram {
     Instrumentation,
@@ -14,130 +17,268 @@ pub enum AssistProgram {
 
 // the function compiling the asssit program to `_tmp_some` executable.
 pub fn compile_assist_program(some_path: Vec<&String>, assist_type: AssistProgram) -> Result<(), String> {
-    let _argus: String = some_path.iter().map(|s| &***s).collect::<Vec<_>>().join(" ");
 
     // get the llvm config lib of the current environment.
-    let llvm_cxx_lib = String::from_utf8(Command::new("llvm-config")
-            .arg("--cxxflags")
-            .arg("--libs")
-            .output().unwrap().stdout).unwrap()
-            .replace("\n", " ")
-            .split(" ")
-            .map(|s| s.replace(" ", ""))
-            .filter(|s| !s.is_empty())
-            .collect::<Vec<_>>();
-    
+    let llvm_cxx_lib = match Command::new("llvm-config").arg("--cxxflags").arg("--libs").output() {
+        Ok(output) => {
+            if !output.stderr.is_empty() {
+                return Err(match String::from_utf8(output.stderr) { Ok(err_msg) => err_msg, Err(e) => e.to_string() })
+            }
+            match String::from_utf8(output.stdout) {
+                Ok(output_str) => output_str
+                                            .replace("\n", " ")
+                                            .split(" ")
+                                            .map(|s| s.replace(" ", ""))
+                                            .filter(|s| !s.is_empty())
+                                            .collect::<Vec<_>>(),
+                Err(e) => return Err(e.to_string())
+            }
+        },
+        Err(e) => return Err(e.to_string())
+    };
+            
     if llvm_cxx_lib.len() == 0 {
         return Err(String::from("LLVM configuration is not set."))
     }
 
-    // compile the assist program through clang++.
-    let compile_res = Command::new("clang++")
-            .arg(_argus)
-            .arg(&llvm_cxx_lib[0])
-            .arg(&llvm_cxx_lib[llvm_cxx_lib.len()-1])
-            .arg("-o")
-            .arg(match assist_type {
-                AssistProgram::Instrumentation => _ASSIST_DIR.to_owned() + _TMP_INS,
-                AssistProgram::Callgraph => _ASSIST_DIR.to_owned() +_TMP_CG,
-                AssistProgram::PathSensitive => _ASSIST_DIR.to_owned() +_TMP_PS
-            }).output().unwrap();
+    // println!("{:?}", llvm_cxx_lib);
 
-    if !compile_res.stderr.is_empty() {
-        Err(String::from_utf8(compile_res.stderr).unwrap())
-    } else {
-        Ok(())
-    }
+    // compile the assist program through clang++.
+    match Command::new("clang++")
+        .args(some_path)
+        .args(&llvm_cxx_lib)
+        .arg("-o")
+        .arg(match assist_type {
+            AssistProgram::Instrumentation => _ASSIST_DIR.to_owned() + _TMP_INS,
+            AssistProgram::Callgraph => _ASSIST_DIR.to_owned() +_TMP_CG,
+            AssistProgram::PathSensitive => _ASSIST_DIR.to_owned() +_TMP_PS
+        }).output() {
+            Ok(output) => {
+                if !output.stderr.is_empty() {
+                    return Err(match String::from_utf8(output.stderr) { Ok(err_msg) => err_msg, Err(e) => e.to_string() })
+                }; 
+                Ok(())
+            },
+            Err(e) => Err(e.to_string())
+        }
     
 }
 
+// check whether or not the assist file exists.
 pub fn exist_assist_file(assist_type: AssistProgram) -> Result<bool, String> {
-    let ls_res = Command::new("ls").arg(_ASSIST_DIR).output().unwrap();
-    if !ls_res.stderr.is_empty() {
-        Err(String::from_utf8(ls_res.stderr).unwrap())
-    } else {
-        let file_list = String::from_utf8(ls_res.stdout).unwrap();
-        match assist_type {
-            AssistProgram::Instrumentation => Ok(file_list.contains(&_TMP_INS)),
-            AssistProgram::Callgraph => Ok(file_list.contains(&_TMP_CG)),
-            AssistProgram::PathSensitive => Ok(file_list.contains(&_TMP_PS))
-        }
+    match Command::new("ls").arg(_ASSIST_DIR).output() {
+        Ok(output) => {
+            if !output.stderr.is_empty() {
+                return Err(match String::from_utf8(output.stderr) { Ok(err_msg) => err_msg, Err(e) => e.to_string() })
+            }
+            match String::from_utf8(output.stdout) {
+                Ok(output_str) => match assist_type {
+                    AssistProgram::Instrumentation => Ok(output_str.contains(&_TMP_INS)),
+                    AssistProgram::Callgraph => Ok(output_str.contains(&_TMP_CG)),
+                    AssistProgram::PathSensitive => Ok(output_str.contains(&_TMP_PS))
+                },
+                Err(e) => Err(e.to_string())
+            }
+        },
+        Err(e) => Err(e.to_string())
     }
 }
 
 // auto-instrumentation
-pub fn instrumentation(input_path: Vec<&str>, output_path: &str) -> Result<(), String> {
+pub fn instrumentation(input_path: Vec<&str>, output_path: &str, cxx_flag: bool) -> Result<(), String> {
 
-    // The IR code files before instrumentation.
+    // use c or cxx compiler.
+    let clang_compiler = if cxx_flag { "clang++" } else { "clang" };
+
+    // the IR code files before instrumentation.
     let mut tmp_ir_paths: Vec<String> = vec![];
-    // The IR code files after instrumentation.
+    // the IR code files after instrumentation.
     let mut tmp_ir_paths_: Vec<String> = vec![];
-    // The object code files after instrumentation.
+    // the object code files after instrumentation.
     let mut tmp_obj_paths: Vec<String> = vec![];
 
     for input in input_path.iter() {
         let path = Path::new(input);
-        let input_name = path.file_stem().unwrap().to_str().unwrap();
-        let input_parent = path.parent().unwrap();
+        let input_name = match path.file_stem() {
+            Some(stem) => match stem.to_str() {
+                Some(stem_str) => stem_str,
+                None => return Err(String::from("Failed to convert &OsStr to &str."))
+            }
+            None => return Err(String::from("Failed to split the input assist file path."))
+        };
+        let input_parent = match path.parent() {
+            Some(parent) => parent,
+            None => return Err(String::from("Failed to get the parent dirs of input assist file path."))
+        };
         let tmp_ir = input_parent.join(input_name.to_owned() + ".ll");
         let tmp_ir_ = input_parent.join("_".to_owned() + input_name + ".ll");
         let tmp_obj = input_parent.join("_".to_owned() + input_name + ".o");
         
         // compile all input files into IR code.
-        let compile_res = Command::new("clang++")
+        match Command::new(clang_compiler)
             .arg("-S")
             .arg("-emit-llvm")
             .arg(input)
             .arg("-o")
-            .arg(tmp_ir.clone()).output().unwrap();
+            .arg(tmp_ir.clone()).output() {
+                Ok(output) => {
+                    if !output.stderr.is_empty() {
+                        return Err(match String::from_utf8(output.stderr) { Ok(err_msg) => err_msg, Err(e) => e.to_string() })
+                    }; 
+                },
+                Err(e) => return Err(e.to_string())
+            };
 
         // perform instrumentation on all IR codes.
-        let instrumentation_res = Command::new(_ASSIST_DIR.to_owned() + _TMP_INS)
+        match Command::new(_ASSIST_DIR.to_owned() + _TMP_INS)
             .arg(tmp_ir.clone())
-            .arg(tmp_ir_.clone()).output().unwrap();
+            .arg(tmp_ir_.clone()).output() {
+                Ok(output) => {
+                    if !output.stderr.is_empty() {
+                        return Err(match String::from_utf8(output.stderr) { Ok(err_msg) => err_msg, Err(e) => e.to_string() })
+                    }; 
+                },
+                Err(e) => return Err(e.to_string())
+            }
 
         // assemble input files to object code.
-        let assemble_res = Command::new("clang++")
+        match Command::new(clang_compiler)
             .arg("-c")
             .arg(tmp_ir_.clone())
             .arg("-o")
-            .arg(tmp_obj.clone()).output().unwrap();
+            .arg(tmp_obj.clone()).output() {
+                Ok(output) => {
+                    if !output.stderr.is_empty() {
+                        return Err(match String::from_utf8(output.stderr) { Ok(err_msg) => err_msg, Err(e) => e.to_string() })
+                    }; 
+                },
+                Err(e) => return Err(e.to_string())
+            }
 
-        if !compile_res.stderr.is_empty() {
-            // remove all temp files.
-            return Err(String::from_utf8(compile_res.stderr).unwrap());
-        } else if !instrumentation_res.stderr.is_empty() {
-            return Err(String::from_utf8(instrumentation_res.stderr).unwrap());
-        } else if !assemble_res.stderr.is_empty() {
-            return Err(String::from_utf8(assemble_res.stderr).unwrap());
-        }
-        
-        tmp_ir_paths.push(tmp_ir.to_str().unwrap().to_string());
-        tmp_ir_paths_.push(tmp_ir_.to_str().unwrap().to_string());
-        tmp_obj_paths.push(tmp_obj.to_str().unwrap().to_string());
+        tmp_ir_paths.push(match tmp_ir.to_str() { Some(str) => str.to_string(), None => return Err(String::from("Failed to convert PathBuf to &str.")) });
+        tmp_ir_paths_.push(match tmp_ir_.to_str() { Some(str) => str.to_string(), None => return Err(String::from("Failed to convert PathBuf to &str.")) });
+        tmp_obj_paths.push(match tmp_obj.to_str() { Some(str) => str.to_string(), None => return Err(String::from("Failed to convert PathBuf to &str.")) });
     }
 
-    let link_res = Command::new("clang++")
-        .arg(tmp_obj_paths.join(" "))
+    // link object files into executable.
+    match Command::new(clang_compiler)
+        .args(&tmp_obj_paths)
         .arg("-o")
-        .arg(output_path).output().unwrap();
+        .arg(output_path).output() {
+            Ok(output) => {
+                if !output.stderr.is_empty() {
+                    return Err(match String::from_utf8(output.stderr) { Ok(err_msg) => err_msg, Err(e) => e.to_string() })
+                }; 
+                match rm_temp_files(tmp_ir_paths) { Ok(_) => (), Err(e) => return Err(e.to_string()) };
+                match rm_temp_files(tmp_ir_paths_) { Ok(_) => (), Err(e) => return Err(e.to_string()) };
+                match rm_temp_files(tmp_obj_paths) { Ok(_) => (), Err(e) => return Err(e.to_string()) };
+                Ok(())
+            },
+            Err(e) => Err(e.to_string())
+        }
+}
 
-    if !link_res.stderr.is_empty() {
-        Err(String::from_utf8(link_res.stderr).unwrap())
-    } else {
-        rm_temp_files(tmp_ir_paths).unwrap();
-        rm_temp_files(tmp_ir_paths_).unwrap();
-        rm_temp_files(tmp_obj_paths).unwrap();
-        Ok(())
+pub fn callgraph(input_path: Vec<&str>, output_path: &str, cxx_flag: bool) -> Result<(), String> {
+    let clang_compiler = if cxx_flag { "clang++" } else { "clang" };
+
+    // IR code files.
+    let mut tmp_ir_paths: Vec<String> = vec![];
+
+    for input in input_path.iter() {
+        let path = Path::new(input);
+        let input_name = match path.file_stem() {
+            Some(stem) => match stem.to_str() {
+                Some(stem_str) => stem_str,
+                None => return Err(String::from("Failed to convert &OsStr to &str."))
+            }
+            None => return Err(String::from("Failed to split the input assist file path."))
+        };
+        let input_parent = match path.parent() {
+            Some(parent) => parent,
+            None => return Err(String::from("Failed to get the parent dirs of input assist file path."))
+        };
+        let tmp_ir = input_parent.join(input_name.to_owned() + ".ll");
+        
+        // compile all input files into IR code.
+        match Command::new(clang_compiler)
+            .arg("-S")
+            .arg("-emit-llvm")
+            .arg(input)
+            .arg("-o")
+            .arg(tmp_ir.clone()).output() {
+                Ok(output) => {
+                    if !output.stderr.is_empty() {
+                        return Err(match String::from_utf8(output.stderr) { Ok(err_msg) => err_msg, Err(e) => e.to_string() })
+                    }
+                },
+                Err(e) => return Err(e.to_string())
+            }
+
+        tmp_ir_paths.push(match tmp_ir.to_str() { Some(str) => str.to_string(), None => return Err(String::from("Failed to convert PathBuf to &str.")) });
     }
 
+    // link IR code files together.
+    match Command::new("llvm-link")
+        .arg("-S")
+        .args(&tmp_ir_paths)
+        .arg("-o")
+        .arg(_LINKED_LL).output() {
+            Ok(output) => {
+                if !output.stderr.is_empty() {
+                    return Err(match String::from_utf8(output.stderr) { Ok(err_msg) => err_msg, Err(e) => e.to_string() })
+                }; 
+            },
+            Err(e) => return Err(e.to_string())
+        }
+    
+    match Command::new(_ASSIST_DIR.to_owned() + _TMP_CG)
+        .arg(_LINKED_LL).output() {
+            Ok(output) => {
+                if !output.stderr.is_empty() {
+                    return Err(match String::from_utf8(output.stderr) { Ok(err_msg) => err_msg, Err(e) => e.to_string() })
+                } else {
+                    match File::create(output_path) {
+                        Ok(mut file) => match file.write_all(&output.stdout) {
+                            Ok(_) => (),
+                            Err(e) => return Err(e.to_string())
+                        },
+                        Err(e) => return Err(e.to_string())
+                    }
+                } 
+            },
+            Err(e) => return Err(e.to_string())
+        }
+
+    match rm_temp_files(tmp_ir_paths) {
+            Ok(_) => (),
+            Err(e) => return Err(e.to_string())
+        }
+
+    match Command::new("rm").arg("-f").arg(_LINKED_LL).output() {
+            Ok(output) => {
+                if !output.stderr.is_empty() {
+                    return Err(match String::from_utf8(output.stderr) { Ok(err_msg) => err_msg, Err(e) => e.to_string() })
+                }; 
+                Ok(())
+            },
+            Err(e) => Err(e.to_string())
+        }
+
+}
+
+pub fn path_sensitive(input_path: Vec<&str>, output_path: &str, cxx_flag: bool) -> Result<(), String> {
+    unimplemented!("unimplemented callgraph, {}, {}, {}", input_path.join(" "), output_path, cxx_flag);
 }
 
 fn rm_temp_files(tmp_files: Vec<String>) -> Result<(), String> {
     for tmp_file in tmp_files {
-        let rm_res = Command::new("rm").arg("-f").arg(tmp_file).output().unwrap();
-        if !rm_res.stderr.is_empty() {
-            return Err(String::from_utf8(rm_res.stderr).unwrap());
+        match Command::new("rm").arg("-f").arg(tmp_file).output() {
+            Ok(output) => {
+                if !output.stderr.is_empty() {
+                    return Err(match String::from_utf8(output.stderr) { Ok(err_msg) => err_msg, Err(e) => e.to_string() })
+                }; 
+            },
+            Err(e) => return Err(e.to_string())
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod assist;
 
-use assist::{AssistProgram, compile_assist_program, instrumentation};
+use assist::{callgraph, compile_assist_program, instrumentation, path_sensitive, AssistProgram};
 use clap::{Arg, ArgAction};
 
 use crate::assist::exist_assist_file;
@@ -9,17 +9,20 @@ use crate::assist::exist_assist_file;
 
 fn main() {
     let mycc = clap::Command::new("mycc")
-        .disable_help_flag(false)
+        // .disable_help_flag(false)
         .arg(Arg::new("ins_path").short('I').long("instrumentation-program-path").num_args(0..))
         .arg(Arg::new("cg_path").short('C').long("callgraph-program-path").num_args(0..))
         .arg(Arg::new("ps_path").short('P').long("path-sensitive-program-path").num_args(0..))
-        .arg(Arg::new("cached").short('c').long("cached-assist-programs").action(ArgAction::SetTrue))
+        .arg(Arg::new("make").short('m').long("use-make").action(ArgAction::SetTrue))
+        .arg(Arg::new("cached").short('c').long("use-cached-assist-programs").action(ArgAction::SetTrue))
+        .arg(Arg::new("cxx_flag").short('x').long("use-cxx-compiler").action(ArgAction::SetTrue))
         .arg(Arg::new("in_path").short('i').long("input-files-path").required(true).num_args(1..))
         .arg(Arg::new("out_path").short('o').long("output-file-path").required(true).num_args(0..));
 
     let matches = mycc.try_get_matches().unwrap_or_else(|e| e.exit());
 
     let cached_flag = matches.get_flag("cached");
+    let cxx_flag = matches.get_flag("cxx_flag");
     let in_path = matches.get_many::<String>("in_path").unwrap().collect::<Vec<_>>().iter().map(|s| (**s).as_str()).collect::<Vec<_>>();
     let out_path = matches.get_one::<String>("out_path").map(|s| (*s).as_str()).unwrap();
 
@@ -30,12 +33,12 @@ fn main() {
             Ok(founded)  => {
                 if !(cached_flag && founded) {
                     match compile_assist_program(ins_path.clone().collect::<Vec<_>>(),AssistProgram::Instrumentation)
-                    { Err(err_msg) => println!("Failed to compile assist program: {}.", err_msg), Ok(_) => () }
-                }
-                match instrumentation(in_path.clone(), out_path)
-                { Err(err_msg) => println!("Failed to perform instrumentation: {}.", err_msg), Ok(_) => () }
+                    { Err(err_msg) => println!("Failed to compile instrumentation program:\n{}", err_msg), Ok(_) => () }
+                } 
+                match instrumentation(in_path.clone(), out_path, cxx_flag)
+                { Err(err_msg) => println!("Failed to perform instrumentation:\n{}", err_msg), Ok(_) => () }
             },
-            Err(err_msg) => println!("Failed to locate assist file: {}", err_msg),
+            Err(err_msg) => println!("Failed to locate assist file:\n{}", err_msg),
         }
 
         // println!("The Instrumentation program path is: {:?}.", ins_path.collect::<Vec<_>>());
@@ -43,25 +46,35 @@ fn main() {
     } else if let Some(cg_path) = matches.get_many::<String>("cg_path") {
         // the assist program is `callgraph`.
 
-        match compile_assist_program(
-            cg_path.clone().collect::<Vec<_>>(),
-            AssistProgram::Callgraph
-        ){
-            Err(err_msg) => println!("Failed to compile assist program: {}.", err_msg),
-            Ok(_) => (),
+        match exist_assist_file(AssistProgram::Callgraph) {
+            Ok(founded)  => {
+                if !(cached_flag && founded) {
+                    match compile_assist_program(cg_path.clone().collect::<Vec<_>>(),AssistProgram::Instrumentation)
+                    { Err(err_msg) => println!("Failed to compile callgraph program:\n{}", err_msg), Ok(_) => () }
+                }
+                match callgraph(in_path.clone(), out_path, cxx_flag)
+                { Err(err_msg) => println!("Failed to generate call graph:\n{}", err_msg), Ok(_) => () }
+            },
+            Err(err_msg) => println!("Failed to locate assist file:\n{}", err_msg),
         }
+
         // println!("The Call graph analyzing program path is: {:?}.", cg_path.collect::<Vec<_>>());
 
     } else if let Some(ps_path) = matches.get_many::<String>("ps_path") {
         // the assist program is `path sensitive`.
 
-        match compile_assist_program(
-            ps_path.clone().collect::<Vec<_>>(),
-            AssistProgram::PathSensitive
-        ){
-            Err(err_msg) => println!("Failed to compile assist program: {}.", err_msg),
-            Ok(_) => (),
+        match exist_assist_file(AssistProgram::PathSensitive) {
+            Ok(founded)  => {
+                if !(cached_flag && founded) {
+                    match compile_assist_program(ps_path.clone().collect::<Vec<_>>(),AssistProgram::Instrumentation)
+                    { Err(err_msg) => println!("Failed to compile path sensitive program:\n{}", err_msg), Ok(_) => () }
+                }
+                match path_sensitive(in_path.clone(), out_path, cxx_flag)
+                { Err(err_msg) => println!("Failed to perform path sensitive analysis:\n{}", err_msg), Ok(_) => () }
+            },
+            Err(err_msg) => println!("Failed to locate assist file:\n{}", err_msg),
         }
+
         // println!("The Path sensitive analyzing program path is: {:?}.", ps_path.collect::<Vec<_>>());
 
     }
