b8b11e3f9f986359dc4e8988e7aac460cb8a9150
gentleGuardian123 <takigawa_yoshino@163.com>
2024-04-27 10:47:39 +0800

    finished mycc instrumentation.

--- src/assist.rs
+++ src/assist.rs
@@ -1,10 +1,10 @@
 use std::process::Command;
 use std::path::Path;
 
-const _TMP_INS: &str = "_tmp_ins";
-const _TMP_CG: &str = "_tmp_cg";
-const _TMP_PS: &str = "_tmp_ps";
-const _TMP_LL: &str = "_tmp_ll.ll";
+const _ASSIST_DIR: &str = "assists/";
+const _TMP_INS: &str = "_instrumentation";
+const _TMP_CG: &str = "_callgraph";
+const _TMP_PS: &str = "_path_sensitive";
 
 pub enum AssistProgram {
     Instrumentation,
@@ -38,9 +38,9 @@ pub fn compile_assist_program(some_path: Vec<&String>, assist_type: AssistProgra
             .arg(&llvm_cxx_lib[llvm_cxx_lib.len()-1])
             .arg("-o")
             .arg(match assist_type {
-                AssistProgram::Instrumentation => _TMP_INS,
-                AssistProgram::Callgraph => _TMP_CG,
-                AssistProgram::PathSensitive => _TMP_PS
+                AssistProgram::Instrumentation => _ASSIST_DIR.to_owned() + _TMP_INS,
+                AssistProgram::Callgraph => _ASSIST_DIR.to_owned() +_TMP_CG,
+                AssistProgram::PathSensitive => _ASSIST_DIR.to_owned() +_TMP_PS
             }).output().unwrap();
 
     if !compile_res.stderr.is_empty() {
@@ -51,12 +51,37 @@ pub fn compile_assist_program(some_path: Vec<&String>, assist_type: AssistProgra
     
 }
 
-pub fn instrumentation(input_path: Vec<&str>, _output_path: &str) -> Result<(), String>{
+pub fn exist_assist_file(assist_type: AssistProgram) -> Result<bool, String> {
+    let ls_res = Command::new("ls").arg(_ASSIST_DIR).output().unwrap();
+    if !ls_res.stderr.is_empty() {
+        Err(String::from_utf8(ls_res.stderr).unwrap())
+    } else {
+        let file_list = String::from_utf8(ls_res.stdout).unwrap();
+        match assist_type {
+            AssistProgram::Instrumentation => Ok(file_list.contains(&_TMP_INS)),
+            AssistProgram::Callgraph => Ok(file_list.contains(&_TMP_CG)),
+            AssistProgram::PathSensitive => Ok(file_list.contains(&_TMP_PS))
+        }
+    }
+}
+
+// auto-instrumentation
+pub fn instrumentation(input_path: Vec<&str>, output_path: &str) -> Result<(), String> {
+
+    // The IR code files before instrumentation.
+    let mut tmp_ir_paths: Vec<String> = vec![];
+    // The IR code files after instrumentation.
+    let mut tmp_ir_paths_: Vec<String> = vec![];
+    // The object code files after instrumentation.
+    let mut tmp_obj_paths: Vec<String> = vec![];
 
     for input in input_path.iter() {
         let path = Path::new(input);
         let input_name = path.file_stem().unwrap().to_str().unwrap();
         let input_parent = path.parent().unwrap();
+        let tmp_ir = input_parent.join(input_name.to_owned() + ".ll");
+        let tmp_ir_ = input_parent.join("_".to_owned() + input_name + ".ll");
+        let tmp_obj = input_parent.join("_".to_owned() + input_name + ".o");
         
         // compile all input files into IR code.
         let compile_res = Command::new("clang++")
@@ -64,28 +89,57 @@ pub fn instrumentation(input_path: Vec<&str>, _output_path: &str) -> Result<(), 
             .arg("-emit-llvm")
             .arg(input)
             .arg("-o")
-            .arg(input_parent.join(input_name.to_owned() + ".ll")).output().unwrap();
+            .arg(tmp_ir.clone()).output().unwrap();
 
         // perform instrumentation on all IR codes.
-        let instrumentation_res = Command::new(_TMP_INS)
-            .arg(input_parent.join(input_name.to_owned() + ".ll"))
-            .arg(input_parent.join("_".to_owned() + input_name + ".ll")).output().unwrap();
+        let instrumentation_res = Command::new(_ASSIST_DIR.to_owned() + _TMP_INS)
+            .arg(tmp_ir.clone())
+            .arg(tmp_ir_.clone()).output().unwrap();
 
         // assemble input files to object code.
         let assemble_res = Command::new("clang++")
             .arg("-c")
-            .arg(input_parent.join("_".to_owned() + input_name + ".ll"))
-            .arg(input_parent.join("_".to_owned() + input_name + ".o")).output().unwrap();
+            .arg(tmp_ir_.clone())
+            .arg("-o")
+            .arg(tmp_obj.clone()).output().unwrap();
 
-        if !compile_res.stderr.is_empty() || !instrumentation_res.stderr.is_empty() || !assemble_res.stderr.is_empty() {
+        if !compile_res.stderr.is_empty() {
             // remove all temp files.
-            return Err(String::from_utf8(compile_res.stderr).unwrap())
+            return Err(String::from_utf8(compile_res.stderr).unwrap());
+        } else if !instrumentation_res.stderr.is_empty() {
+            return Err(String::from_utf8(instrumentation_res.stderr).unwrap());
+        } else if !assemble_res.stderr.is_empty() {
+            return Err(String::from_utf8(assemble_res.stderr).unwrap());
         }
-
+        
+        tmp_ir_paths.push(tmp_ir.to_str().unwrap().to_string());
+        tmp_ir_paths_.push(tmp_ir_.to_str().unwrap().to_string());
+        tmp_obj_paths.push(tmp_obj.to_str().unwrap().to_string());
     }
 
-    let _tmp_object = input_path.iter().map(|s| Path::new(*s).parent().unwrap().join(Path::new(*s).file_stem().unwrap().to_str().unwrap().to_owned() + ".o")).collect::<Vec<_>>();
+    let link_res = Command::new("clang++")
+        .arg(tmp_obj_paths.join(" "))
+        .arg("-o")
+        .arg(output_path).output().unwrap();
 
+    if !link_res.stderr.is_empty() {
+        Err(String::from_utf8(link_res.stderr).unwrap())
+    } else {
+        rm_temp_files(tmp_ir_paths).unwrap();
+        rm_temp_files(tmp_ir_paths_).unwrap();
+        rm_temp_files(tmp_obj_paths).unwrap();
+        Ok(())
+    }
+
+}
+
+fn rm_temp_files(tmp_files: Vec<String>) -> Result<(), String> {
+    for tmp_file in tmp_files {
+        let rm_res = Command::new("rm").arg("-f").arg(tmp_file).output().unwrap();
+        if !rm_res.stderr.is_empty() {
+            return Err(String::from_utf8(rm_res.stderr).unwrap());
+        }
+    }
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,48 @@
 mod assist;
 
 use assist::{AssistProgram, compile_assist_program, instrumentation};
-use clap::Arg;
+use clap::{Arg, ArgAction};
+
+use crate::assist::exist_assist_file;
 
 // const MAX_FILE_ARGS_NUM: usize = 5;
 
 fn main() {
     let mycc = clap::Command::new("mycc")
         .disable_help_flag(false)
-        .arg(Arg::new("ins_path").short('I').long("instrumentation_program_path").num_args(1..))
-        .arg(Arg::new("cg_path").short('C').long("callgraph_program_path").num_args(1..))
-        .arg(Arg::new("ps_path").short('P').long("path_sensitive_program_path").num_args(1..))
-        .arg(Arg::new("in_path").short('i').long("input_file_path").required(true).num_args(1..))
-        .arg(Arg::new("out_path").short('o').long("output_file_path").required(true));
+        .arg(Arg::new("ins_path").short('I').long("instrumentation-program-path").num_args(0..))
+        .arg(Arg::new("cg_path").short('C').long("callgraph-program-path").num_args(0..))
+        .arg(Arg::new("ps_path").short('P').long("path-sensitive-program-path").num_args(0..))
+        .arg(Arg::new("cached").short('c').long("cached-assist-programs").action(ArgAction::SetTrue))
+        .arg(Arg::new("in_path").short('i').long("input-files-path").required(true).num_args(1..))
+        .arg(Arg::new("out_path").short('o').long("output-file-path").required(true).num_args(0..));
 
     let matches = mycc.try_get_matches().unwrap_or_else(|e| e.exit());
 
+    let cached_flag = matches.get_flag("cached");
     let in_path = matches.get_many::<String>("in_path").unwrap().collect::<Vec<_>>().iter().map(|s| (**s).as_str()).collect::<Vec<_>>();
     let out_path = matches.get_one::<String>("out_path").map(|s| (*s).as_str()).unwrap();
 
     if let Some(ins_path) = matches.get_many::<String>("ins_path") {
-
         // the assist program is `instrumentation`.
-        match compile_assist_program(
-            ins_path.clone().collect::<Vec<_>>(),
-            AssistProgram::Instrumentation
-        ){
-            Err(err_msg) => println!("Failed to compile assist program: {}.", err_msg),
-            Ok(_) => instrumentation(in_path.clone(), out_path).unwrap(),
+
+        match exist_assist_file(AssistProgram::Instrumentation) {
+            Ok(founded)  => {
+                if !(cached_flag && founded) {
+                    match compile_assist_program(ins_path.clone().collect::<Vec<_>>(),AssistProgram::Instrumentation)
+                    { Err(err_msg) => println!("Failed to compile assist program: {}.", err_msg), Ok(_) => () }
+                }
+                match instrumentation(in_path.clone(), out_path)
+                { Err(err_msg) => println!("Failed to perform instrumentation: {}.", err_msg), Ok(_) => () }
+            },
+            Err(err_msg) => println!("Failed to locate assist file: {}", err_msg),
         }
+
         // println!("The Instrumentation program path is: {:?}.", ins_path.collect::<Vec<_>>());
 
     } else if let Some(cg_path) = matches.get_many::<String>("cg_path") {
-
         // the assist program is `callgraph`.
+
         match compile_assist_program(
             cg_path.clone().collect::<Vec<_>>(),
             AssistProgram::Callgraph
@@ -44,8 +53,8 @@ fn main() {
         // println!("The Call graph analyzing program path is: {:?}.", cg_path.collect::<Vec<_>>());
 
     } else if let Some(ps_path) = matches.get_many::<String>("ps_path") {
-
         // the assist program is `path sensitive`.
+
         match compile_assist_program(
             ps_path.clone().collect::<Vec<_>>(),
             AssistProgram::PathSensitive
@@ -56,8 +65,6 @@ fn main() {
         // println!("The Path sensitive analyzing program path is: {:?}.", ps_path.collect::<Vec<_>>());
 
     }
-
-    println!("The input and output file path is: {:?} & {}.", in_path, out_path);
 
 }
 
